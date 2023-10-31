66f30589657e13ef1b9e2b6bbc3096c87a9b4105
idomingexp <144793269+idomingexp@users.noreply.github.com>
2023-10-31 13:32:35 +0100

    Update Ejercicio 4.py

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -1,6 +1,8 @@
-a = input("Dime los numeros ganadores de la loteria separados por espacios:")
-a = a.split(" ")
-for x in range(len(a)):
-    print(min(a))
-    a.remove(min(a))
+n = int(input("Dime cuantos Nº ganadores hay:"))
+a = []
+for x in range(n):
+    b = int(input("Dime un número ganador:"))
+    a.append(b)
+a.sort()
+print(a)
 input()
